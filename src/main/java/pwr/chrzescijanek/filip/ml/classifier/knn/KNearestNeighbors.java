46023fc38e66f03d164b4a808c74cbb6b44f9a52
Filip Chrześcijanek <filip702@gmail.com>
2017-12-08 21:50:32 +0100

    kNN without attribute values normalization

--- src/main/java/pwr/chrzescijanek/filip/ml/classifier/knn/KNearestNeighbors.java
+++ src/main/java/pwr/chrzescijanek/filip/ml/classifier/knn/KNearestNeighbors.java
@@ -1,19 +1,78 @@
 package pwr.chrzescijanek.filip.ml.classifier.knn;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+import javafx.util.Pair;
 import pwr.chrzescijanek.filip.ml.classifier.AbstractClassifier;
 import pwr.chrzescijanek.filip.ml.data.DataSet;
+import pwr.chrzescijanek.filip.ml.data.attribute.DiscreteAttribute;
+import pwr.chrzescijanek.filip.ml.data.record.Record;
 import pwr.chrzescijanek.filip.ml.data.record.TestRecord;
 
 public class KNearestNeighbors extends AbstractClassifier {
 
+	private final Integer k;
+	private final BiFunction<TestRecord, Record, Double> function;
+
+	private DataSet dataSet;
+
+	public KNearestNeighbors(final Integer k, final BiFunction<TestRecord, Record, Double> function) {
+		this.k = Objects.requireNonNull(k);
+		this.function = Objects.requireNonNull(function);
+	}
+	
 	@Override
 	protected void buildModel(final DataSet tds) {
+		dataSet = minimalizeDifferences(tds);
+	}
 
+	private DataSet minimalizeDifferences(DataSet tds) {
+		checkAttributes(tds);
+		return tds;
+	}
+
+	private void checkAttributes(final DataSet tds) {
+		tds.getAttributes().forEach(a -> {
+			if (a instanceof DiscreteAttribute) {
+				throw new IllegalArgumentException("Attributes must have continuous values!");
+			}
+		});
 	}
 
 	@Override
 	protected void assignClass(final TestRecord tr) {
+		tr.setAssignedClazz(dataSet.getRecords()
+				.parallelStream()
+				.map(r -> new Pair<String, Double>(r.getClazz(), function.apply(tr, r)))
+				.sorted((p1, p2) -> p1.getValue().compareTo(p2.getValue()))
+				.limit(k)
+				.collect(Collectors.groupingBy(pair -> pair.getKey(), Collectors.counting()))
+				.entrySet()
+				.stream()
+				.max((e1, e2) -> e1.getValue().compareTo(e2.getValue()))
+				.orElse(new NullEntry()).getKey());
+	}
 
+	private final class NullEntry implements Map.Entry<String, Long> {
+		
+		@Override
+		public String getKey() {
+			return null;
+		}
+
+		@Override
+		public Long getValue() {
+			return 0L;
+		}
+
+		@Override
+		public Long setValue(Long value) {
+			return 0L;
+		}
+		
 	}
 
 }
--- src/test/java/pwr/chrzescijanek/filip/ml/classifier/ClassifierTest.java
+++ src/test/java/pwr/chrzescijanek/filip/ml/classifier/ClassifierTest.java
@@ -11,26 +11,36 @@ import pwr.chrzescijanek.filip.ml.data.discretizer.EntropyDiscretizer;
 import pwr.chrzescijanek.filip.ml.data.discretizer.RangeDiscretizer;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ClassifierTest {
 
 	@Test
 	public void bayes() throws Exception {
-		testCrossValidation(new Bayes(), false);
+		testCrossValidation(new Bayes(), false, false);
 	}
 
 	@Test	
 	public void knn() throws Exception {
-		testCrossValidation(new KNearestNeighbors(), false);
+		testCrossValidation(new KNearestNeighbors(5, (r1, r2) -> {
+			Double distance = 0.0;
+			List<Double> firstValues  = r1.getRawValues().stream().map(o -> (Double) o).collect(Collectors.toList());
+			List<Double> secondValues = r2.getRawValues().stream().map(o -> (Double) o).collect(Collectors.toList());
+			for (int i = 0; i < firstValues.size(); i++) {
+				distance += Math.pow(firstValues.get(i) - secondValues.get(i), 2);
+			}
+			return Math.sqrt(distance);
+		}), false, true);
 	}
 
 	@Test
 	public void ila() throws Exception {
-		testCrossValidation(new InductiveLearning(), true);
+		testCrossValidation(new InductiveLearning(), true, false);
 	}
 
-	private void testCrossValidation(final Classifier c, final Boolean onlyDiscrete) throws IOException {
+	private void testCrossValidation(final Classifier c, final Boolean onlyDiscrete, final Boolean onlyContinuous) throws IOException {
 		final Integer classIndex = 0;
 		final Integer folds      = 10;
 		final String dataSet     = getClass().getResource("/iris.csv").getPath();
@@ -38,9 +48,11 @@ public class ClassifierTest {
 		if (!onlyDiscrete) {
 			print(c.crossValidate(new DataSource(dataSet, classIndex).asDataSet(), folds));
 		}
-		print(c.crossValidate(new DataSource(dataSet, classIndex).asDataSet(new RangeDiscretizer()), folds));
-		print(c.crossValidate(new DataSource(dataSet, classIndex).asDataSet(new CardinalityDiscretizer()), folds));
-		print(c.crossValidate(new DataSource(dataSet, classIndex).asDataSet(new EntropyDiscretizer()), folds));
+		if (!onlyContinuous) {
+			print(c.crossValidate(new DataSource(dataSet, classIndex).asDataSet(new RangeDiscretizer()), folds));
+			print(c.crossValidate(new DataSource(dataSet, classIndex).asDataSet(new CardinalityDiscretizer()), folds));
+			print(c.crossValidate(new DataSource(dataSet, classIndex).asDataSet(new EntropyDiscretizer()), folds));
+		}
 	}
 
 	private void print(final Eval e) {
